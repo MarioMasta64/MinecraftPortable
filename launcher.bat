6c9245636a67c0165b53b2a48cbc97285ee65474
Isaiah Love <MarioMasta64@users.noreply.github.com>
2017-02-09 18:55:30 -0500

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -304,7 +304,7 @@ goto UPDATE FAILED
 :REPLACERCREATE
 cls
 echo del launcher.bat > replacer.bat
-echo rename launcher.bat.1 replacer.bat
+echo rename launcher.bat.1 launcher.bat >> replacer.bat
 echo start launcher.bat >> replacer.bat
 echo move version.txt %CD%\doc\version.txt >> replacer.bat
 echo del version.txt.1 >> replacer.bat
